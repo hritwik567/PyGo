2d33f1dbafbd62bab909d57fa7c2c2195d2956fb
Arpit Gupta <arpitguptarag@gmail.com>
2019-02-14 12:57:29 +0530

    Add new test cases

--- ass2/tests/arithmetic_computations.go
+++ ass2/tests/arithmetic_computations.go
@@ -4,10 +4,10 @@ import "fmt";
 func main() {
 	 if 7%2 == 0 {
 	    fmt.Println("7 is even");
-		    // 
+		    //
 		} else {
 		     fmt.Println("7 is odd");
-				    // 
+				    //
 		}
 		    X:= 2*3 + 5*6;
 			    Y:= 7%5;
