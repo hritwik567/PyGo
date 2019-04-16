3969bdcd769eff019ef5808fe7c09ef4af4877c1
Hritvik Taneja <hritwik567@gmail.com>
2019-04-16 22:16:07 +0530

    Add testcase 20

---
+++ ass5/tests/20_function_param_return.go
@@ -0,0 +1,32 @@
+package main
+
+type List struct {
+  	val int
+  	next *type List
+}
+
+func foo(a type List) int {
+	c := a.val
+	return c
+}
+
+func node(a type List) *type List {
+  var b *type List = malloc(8)
+  b[0].val = a.val + 1
+  b[0].next = nil
+  return b
+}
+
+func main() {
+  var head type List
+  head.val = 1
+  head.next = nil
+  b := foo(head)
+  printf("%d\n", b)
+
+  var root *type List
+  root = node(head)
+  printf("%d\n", root[0].val)
+
+  printf("Hritvik done\n")
+}
