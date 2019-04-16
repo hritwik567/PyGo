b962c5d5964c0db8d8cf1a684a0608b800ce2f60
Arpit Gupta <arpitguptarag@gmail.com>
2019-04-16 23:01:11 +0530

    Minor bug found

--- ass5/tests/17_tree_traversal.go
+++ ass5/tests/17_tree_traversal.go
@@ -16,25 +16,25 @@ func new_node(val int) *type node {
 
 func insert_node(head *type node, val int) *type node {
 	
-	printf("Insert node\n")
-	printf("1\n")
+	// printf("Insert node\n")
+	// printf("1\n")
 	if head == nil {
 		return new_node(val)
 	}
 
 	temp := *head
 
-	printf("2\n")
+	// printf("2\n")
 	if val < temp.v {
-		printf("3.a\n")
+		// printf("3.a\n")
 		head[0].l = insert_node(temp.l, val)
 	} else if val > temp.v {
-		printf("3.b\n")
+		// printf("3.b\n")
 		head[0].r = insert_node(temp.r, val)
 	}
 
 	head[0] = temp
-	printf("4\n")
+	// printf("4\n")
 	return head;
 }
 
@@ -62,24 +62,27 @@ func search(root *type node, key int) *type node {
 func main(){
 	head := new_node(3)
 	head = insert_node(head, 5)
-	printf("head: %lx\n", head);
-	printf("head val: %lx\n", head[0].v);
-	printf("head left: %lx\n", head[0].l);
-	printf("head right: %lx\n", head[0].r);
+	// printf("head: %lx\n", head);
+	// printf("head val: %lx\n", head[0].v);
+	// printf("head left: %lx\n", head[0].l);
+	// printf("head right: %lx\n", head[0].r);
 	
-	head = insert_node(head, 2)
-	head = insert_node(head, 8)
-	head = insert_node(head, 1)
+	// head = insert_node(head, 2)
+	// head = insert_node(head, 8)
+	// head = insert_node(head, 1)
 
 
 	printf("head val: %lx\n", head[0].v);
 	printf("head left: %lx\n", head[0].l);
 	printf("head right: %lx\n", head[0].r);
 
-	t3 := head[0]
-	t4 := t3.r
+	var t3 type node = *head
+	printf("head val: %lx\n", t3.v);
+	printf("head val: %lx\n", t3.l);
+	printf("head val: %lx\n", t3.r);
+	t4 := t3.l
 	 
-	printf("head val: %lx\n", t4[0].v);
+	// printf("head val: %lx\n", t4[0].v);
 	// printf("head left: %lx\n", t4[0].l);
 	// printf("head right: %lx\n", t4[0].r);
 	// 
